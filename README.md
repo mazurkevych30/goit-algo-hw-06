b6c18378c36230184d57a51ef62104fd42dcf8db
mazurkevych30 <132198562+mazurkevych30@users.noreply.github.com>
2025-02-09 02:06:46 +0000

    add results for task 2 in reame.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # goit-algo-hw-06
+
+## Результати:
+
+**BFS:**
+Birmingham -> Nottingham -> Stafford -> Sheffield -> Derby -> Stoke-on-Trent -> Leeds -> Liverpool -> Manchester -> Southport
+
+**DFS:**
+Birmingham -> Nottingham -> Derby -> Stafford -> Stoke-on-Trent -> Liverpool -> Manchester -> Leeds -> Sheffield -> Southport
+
+## Висновок:
+
+Результати пошуку в ширину відрізняються від результатів пошуку в глибину через різницю у алгоритмі обходу графа.
+
+Пошук у глибину (DFS) виконується шляхом відвідування вершини, а потім за допомогою стеку відвідує всі сусідні вершини, які ще не були відвідані.
+
+Пошук у ширину (BFS) відрізняється від DFS тим, що він відвідує всі вершини на певному рівні перед тим, як перейти до наступного рівня.
--- task_2.py
+++ task_2.py
@@ -1,21 +1,3 @@
-"""
-Результати:
-BFS:
-Birmingham Nottingham Stafford Sheffield Derby Stoke-on-Trent Leeds Liverpool Manchester Southport 
-
-DFS:
-Birmingham Nottingham Derby Stafford Stoke-on-Trent Liverpool Manchester Leeds Sheffield Southport
-
-Висновок: результати пошуку в ширину відрізняються від результатів пошуку в глибину через різницю 
-у алгоритмі обходу графа.
-
-Пошук у глибину (DFS) виконується шляхом відвідування вершини, а потім за допомогою стеку відвідує 
-всі сусідні вершини, які ще не були відвідані.
-
-Пошук у ширину (BFS) відрізняється від DFS тим, що він відвідує всі вершини на певному рівні перед
-тим, як перейти до наступного рівня. 
-"""
-
 from algorithm.bfs import bfs_iterative
 from algorithm.dfs import dfs_iterative
 from task_1 import G
